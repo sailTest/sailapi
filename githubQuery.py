7fdb4b9ca7e8cc5a59fb7e6f3e203d1944afa7a0
Aldo <aldo.mestas@gmail.com>
2024-01-16 07:01:14 -0600

    counts and details modified

--- githubQuery.py
+++ githubQuery.py
@@ -31,15 +31,23 @@ def get_pull_requests_details(owner, repo, token):
     # Fetch pull requests from the GitHub API
     response = requests.get(base_url, headers=headers, params=params)
 
-    # Check if the request was successful
+    # Check if the request was successful, then capture the data in json format
     if response.status_code == 200:
         pull_requests = response.json()
-
+       
         # Sort pull requests by state (opened, in progress, closed)
         sorted_pull_requests = sorted(pull_requests, key=lambda pr: pr['state'])
 
-        # Print detailed information for each pull request, taking advantage of json based data
-        print(f'Detailed information for {owner}/{repo} pull requests in the last week:')
+
+        # Print detailed information for each pull request
+        print(f'Detailed information for {owner} / {repo} pull requests in the last week:')
+        
+        #Start the counting for PR's state at zero
+        open_pr = 0 
+        closed_pr = 0
+        merged_pr = 0 
+        
+        # Looping through the json dict getting the desired fields
         for pr in sorted_pull_requests:
             print(f'\nPull Request #{pr["number"]}')
             print(f'Title: {pr["title"]}')
@@ -47,7 +55,21 @@ def get_pull_requests_details(owner, repo, token):
             print(f'Created At: {pr["created_at"]}')
             print(f'Updated At: {pr["updated_at"]}')
             print(f'Closed At: {pr["closed_at"]}')
-            print(f'State: {pr["state"]}')
+            print(f'State: {pr["state"]}')  
+
+            #Once this has been collected, it can be included on a dict for further usage      
+            if pr["state"] == "open":
+                open_pr = open_pr + 1 
+            elif  pr["state"] == "closed":
+                closed_pr = closed_pr + 1
+            elif pr["state"] == "merged":
+                merged_pr = merged_pr + 1
+        
+        print(f"\n\nTotal number of PR, Open, Merged, Closed PR for {owner} / {repo}:\n")
+        print(f"Total number of PR: {len(sorted_pull_requests)}")
+        print(f"Open PR = {open_pr}")
+        print(f"Merged PR = {merged_pr}")
+        print(f"Closed PR = {closed_pr}")
     else:
         print(f'Error: Unable to fetch data from GitHub API. Status code: {response.status_code}')
 
