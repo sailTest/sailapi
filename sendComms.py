c784032a042a775677514e0612da1f260c303813
Aldo <aldo.mestas@gmail.com>
2024-01-16 20:00:13 -0600

    token and adjustments done

--- sendComms.py
+++ sendComms.py
@@ -32,23 +32,10 @@ def emailCompose(subject,filename):
     smtp_server = "smtp-mail.outlook.com"
     smtp_port = 587
     smtp_username = "al_785@hotmail.com"
-    smtp_password = "(#t3chn01sl0v3)"
+    smtp_password = ""
     with open(filename,"r") as attachinfo:
         message_bdy = attachinfo.read()
-
+    attachinfo.close()
 
     sendEmail(sender_email, receiver_email, subject,message_bdy, smtp_server, smtp_port, smtp_username, smtp_password)
     
-
-if __name__ == "__main__":
-    # Replace these values with your email and SMTP server information
-    sender_email = "al_785@hotmail.com"
-    receiver_email = "alden.97@gmail.com"
-    #subject = "L"
-    #message_body = "This is the body of the email, and a test."
-    smtp_server = "smtp-mail.outlook.com"
-    smtp_port = 587
-    smtp_username = "al_785@hotmail.com"
-    smtp_password = ""
-
-    sendEmail(sender_email, receiver_email, emailCompose[1],emailCompose(2), smtp_server, smtp_port, smtp_username, smtp_password)
